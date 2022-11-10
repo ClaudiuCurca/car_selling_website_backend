ed6a22df1d057ecd7fd7e9cb45a83788ce14a22d
unknown <curcaclaudiu2000@gmail.com>
2022-11-10 10:05:02 +0200

    fourth commit

--- listings/api/views.py
+++ listings/api/views.py
@@ -1,8 +1,48 @@
+from rest_framework import filters
 from rest_framework import generics
 from .serializers import ListingSerializer
 from listings.models import Listing
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ListingList(generics.ListAPIView):
     queryset = Listing.objects.all().order_by('-date_posted')
     serializer_class = ListingSerializer
+
+
+class ListingCreate(generics.CreateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class ListingDetail(generics.RetrieveAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class ListingDelete(generics.DestroyAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class ListingUpdate(generics.UpdateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class ListingListSearch(generics.ListAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    filter_backends = [filters.OrderingFilter,
+                       DjangoFilterBackend, ]
+    ordering_fields = ['date_posted', 'price']
+
+    filterset_fields = {
+        'make': ['in'],
+        'car_model': ['iexact'],
+        'body_type': ['iexact'],
+        'engine': ['iexact'],
+        'price': ['gte', 'lte'],
+        'fabrication_year': ['gte', 'lte'],
+        'location': ['iexact']
+    }
--- listings/models.py
+++ listings/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Listing(models.Model):
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     choices_body_type = (
@@ -53,9 +57,19 @@ class Listing(models.Model):
     mileage = models.IntegerField()
     price = models.IntegerField()
     location = models.CharField(max_length=30)
-    date_posted = models.DateField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
     picture1 = models.ImageField(
         blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture2 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture3 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture4 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    picture5 = models.ImageField(
+        blank=True, null=True, upload_to="pictures/%Y/%m/%d/")
+    seller_name = models.CharField(max_length=30)
+    seller_phone_number = models.CharField(max_length=30)
 
     def __str__(self):
         return self.title
