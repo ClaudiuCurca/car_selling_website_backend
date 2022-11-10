8bf11884bbad000709348ec29d4a60a5f239c3ee
unknown <curcaclaudiu2000@gmail.com>
2022-11-10 03:04:00 +0200

    third commit

---
+++ users/api/views.py
@@ -0,0 +1,15 @@
+from users.models import Profile
+from .serializers import ProfileSerializer
+from rest_framework import generics
+
+
+class ProfileDetail(generics.RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = 'seller'
+
+
+class ProfileUpdate(generics.UpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    lookup_field = 'seller'
--- users/models.py
+++ users/models.py
@@ -1,4 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    # can't be the same email for 2 users
+    email = models.EmailField(unique=True)
+
+# Creating a one to one relationship to add info about a User
+
+
+class Profile(models.Model):
+    # if a user is deleted means also its profile is deleted
+    seller = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=25, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/%Y/%m/%d/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Profile of {self.seller.username}"
